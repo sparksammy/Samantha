9beb48cecb02d750e8c20557d80ca97b73835f39
Samuel Lord <sllord@icloud.com>
2019-06-04 14:35:42 -0500

    BIG Update
    * Add Limited Roku Support
    * Add STT
    * Minor bug fixes and Linux support updates
    * Give the people proper credit...

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
-#Written by Alex I. Ramirez @alexram1313
-#arcompware.com
+#TTS Engine Written by Alex I. Ramirez @alexram1313
+#Pretty much the rest made by Sparksammy (Samuel Lord)
 import re
 import wave
 import pyaudio
@@ -8,13 +8,16 @@ import time
 import sys, glob
 import aiml
 import os
-import wikipedia #Add the info services here, separated by comma space.
+import wikipedia
+import speech_recognition as sr
+from roku import Roku
 
 kernel = aiml.Kernel()
 for file in glob.glob('ai/*.aiml'):
 	kernel.learn(file)
 
 kernel.respond("LOAD AIML B")
+
 
 class TextToSpeech:
     
@@ -72,27 +75,58 @@ class TextToSpeech:
 if __name__ == '__main__':
     tts = TextToSpeech()
     while True:
-        question = input("S> ")
-        if "open paint" in question: 
-            output = "Opening paint..."
+        # obtain audio from the microphone
+        try:
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Samantha - Ready.")
+                tts.get_pronunciation("I am ready for a request.")
+                recogntionAudio = r.listen(source)
+                question = r.recognize_google(recogntionAudio)
+                print(question)
+        except sr.UnknownValueError:
+            print("Samantha - could not understand audio.")
+            tts.get_pronunciation("Sorry, I couldn't understand you.")
+            question = "wellpoowecantunderstandaudiosoyeahthatsaproblem."
+        except sr.RequestError as e:
+            print("Samantha - could not connect to audio recognition servers.")
+            tts.get_pronunciation("Sorry, I couldn't connect to the audio recogntion service.")
+            question = "wellpoowecantunderstandaudiosoyeahthatsaproblem."
+        if "open gimp" in question: 
+            output = "Opening gimp..."
             print(output)
             response = tts.get_pronunciation(output)
-            os.system('mspaint')
+            os.system('gimp')
+        elif "open mousepad" in question: 
+            output = "Opening mousepad..."
+            print(output)
+            response = tts.get_pronunciation(output)
+            os.system('mousepad')
         elif "open notepad" in question: 
             output = "Opening notepad..."
             print(output)
             response = tts.get_pronunciation(output)
             os.system('notepad')
+        elif "open paint" in question: 
+            output = "Opening paint..."
+            print(output)
+            response = tts.get_pronunciation(output)
+            os.system('paint')
         elif "open command prompt" in question: 
             output = "Opening command prompt..."
             print(output)
             response = tts.get_pronunciation(output)
             os.system('cmd')
+        elif "open terminal" in question: 
+            output = "Opening terminal..."
+            print(output)
+            response = tts.get_pronunciation(output)
+            os.system('xterm')
         elif "open firefox" in question: 
             output = "Opening Fire fox..."
             print(output)
             response = tts.get_pronunciation(output)
-            os.system('firefox.exe')
+            os.system('firefox')
         elif "open chrome" in question: 
             output = "Opening chrome..."
             print(output)
@@ -111,11 +145,70 @@ if __name__ == '__main__':
             output = "Sorry, but I am already taken"
             print(output)
             response = tts.get_pronunciation(output)
-        elif "wikipedia" in question: 
-            wikipedia_q = input("*#W* ")
-            output = wikipedia.summary(wikipedia_q);
-            print(output)
-            response = tts.get_pronunciation(output)
+        elif "Wikipedia" in question: 
+            try:
+                print("Wikipedia - Ready.")
+                tts.get_pronunciation("Wikipedia is ready for a request.")
+                recogntionAudio = r.listen(source)
+                output = wikipedia.summary(recogntionAudio);
+                print(output)
+                response = tts.get_pronunciation(output)
+            except sr.UnknownValueError:
+                print("Wikipedia - could not understand audio.")
+                tts.get_pronunciation("Sorry, I couldn't understand you.")
+            except sr.RequestError as e:
+                 print("Wikipedia - could not connect to audio recognition servers.")
+                 tts.get_pronunciation("Sorry, I couldn't connect to the audio recogntion service.")
+        elif "connect" in question and "Roku" in question: 
+            Roku.discover(timeout=10)
+            print("Roku connect - Ready.")
+            tts.get_pronunciation("Please use the keyboard to type the IP")
+            rokuIP = input("ROKU IP:")
+            roku = Roku(rokuIP)
+        elif "Roku" in question and "home" in question:
+            try:
+                print("Doing roku function.")
+                tts.get_pronunciation("OK. Going to roku home screen.")
+                roku.home()
+            except:
+                print("ERROR. I don't think you are connected...")
+                tts.get_pronunciation("Whoops! Are you sure you are connected?")
+        elif "Roku" in question and "Hulu" in question:
+            try:
+                print("Doing roku function.")
+                tts.get_pronunciation("OK. Going to Hulu.")
+                app = roku['Hulu']
+                app.launch()
+            except:
+                print("ERROR. I don't think you are connected...")
+                tts.get_pronunciation("Whoops! Are you sure you are connected?")
+        elif "Roku" in question and "Netflix" in question:
+            try:
+                print("Doing roku function.")
+                tts.get_pronunciation("OK. Going to Netflix.")
+                app = roku['Netflix']
+                app.launch()
+            except:
+                print("ERROR. I don't think you are connected...")
+                tts.get_pronunciation("Whoops! Are you sure you are connected?")
+        elif "Roku" in question and "Youtube" in question:
+            try:
+                print("Doing roku function.")
+                tts.get_pronunciation("OK. Going to YouTube.")
+                app = roku['YouTube']
+                app.launch()
+            except:
+                print("ERROR. I don't think you are connected...")
+                tts.get_pronunciation("Whoops! Are you sure you are connected?")
+        elif "Roku" in question and "IPTV" in question:
+            try:
+                print("Doing roku function.")
+                tts.get_pronunciation("OK. Launching Free View Player.")
+                app = roku['FreeView Player']
+                app.launch()
+            except:
+                print("ERROR. I don't think you are connected...")
+                tts.get_pronunciation("Whoops! Are you sure you are connected?")
         else:
             output = kernel.respond(question)
             print(output)
